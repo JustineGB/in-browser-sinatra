08983ed4fdec47348b0b2717a4575ca0ee96a5bc
Justine B <justinegayton@gmail.com>
2018-11-20 17:39:25 +0000

    change users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  
+  get '/users/:id' do
+    if !logged_in?
+      redirect '/bags'
+    end
+
+    @user = User.find(params[:id])
+    if !@user.nil? && @user == current_user
+      erb :'users/show'
+    else
+      redirect '/bags'
+    end
+  end
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/clubs'
+    end
+  end
+
   
 end 
