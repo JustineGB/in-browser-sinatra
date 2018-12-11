ad8ec7b3783f7f951e42f610a807c0d64cb42208
JustineGB <justinegayton@gmail.com>
2018-12-11 13:54:23 -0700

    udpdate seed file"
    '

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,10 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def logged_in?
-      !!current_user      #!!session[:user_id]
+      !!current_user
     end
 
     def current_user
-      #User.find(session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@ Wine.create(name: "Sauvignon Blanc", vineyard: "Cakebread", year: 2010, rating: 
 sally.wines.create(name: "Pinot Noir", vineyard: "Cakebread", year: 2017, rating: 7, tasting_notes: "Elegant, earthy, short")
 sandra_rating = sandra.wines.build(name: "Pinot Grigio", vineyard: "J Vineyards", year: 2014, rating: 8, tasting_notes: "Pear, rose floral, sharp")
 sandra_rating.save
+
+
+Bob = User.create(username: "Bob", email: "bob@email.com", password: "pass1")
+Wine.create(name: "Sauvignon Blanc", vineyard: "Vineyard Vines MV", year: 2014, rating: 7, tasting_notes: "light and refreshing!",  user_id: bob.id)
+
+bob.wines.create(name: "Pinot Noir", vineyard: "Cakebread", year: 2015, rating: 9, tasting_notes: "c'est tres bon!")
